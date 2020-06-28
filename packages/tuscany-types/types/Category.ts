0500a1228271b4e32cdfbe74663afdf29c845c7a
Jake Elder <jake@jakes.world>
2020-06-28 17:00:47 +0700

    chore(types): add id to Category

--- packages/tuscany-types/types/Category.ts
+++ packages/tuscany-types/types/Category.ts
@@ -1,11 +1,10 @@
 import { optional, object, string, array, StructType } from "superstruct";
 
 const Struct = object({
+  id: string(),
   name: string(),
   color: string(),
   places: optional(array(string())),
 });
-
-// Feature
 
 export type Category = StructType<typeof Struct>;

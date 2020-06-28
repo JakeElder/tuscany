eee21d1f26645839588c8e6b23bda267ad44b9e6
Jake Elder <jake@jakes.world>
2020-06-28 22:00:49 +0700

    fix(components): ensure id passed to CategoryListItem story

--- packages/tuscany-components/stories/CategoryListItem.stories.tsx
+++ packages/tuscany-components/stories/CategoryListItem.stories.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import CategoryListItem from "../src/CategoryListItem";
+import { Category } from "@mindfulstudio/tuscany-types/Category";
 
 export default {
   title: "CategoryListItem",
   component: CategoryListItem,
 };
 
-export const Default = () => (
-  <CategoryListItem name="Food and Drink" color="blue" />
-);
+const category: Category = {
+  id: "1",
+  name: "Food and Drink",
+  color: "blue",
+};
+
+export const Default = () => <CategoryListItem {...category} />;
